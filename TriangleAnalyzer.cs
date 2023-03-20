858142a9dae6dc23cb03770208a30cd03f641b2c
Brendan <bcassidy1@uclan.ac.uk>
2023-03-20 23:27:12 +0000

    Added comments to method names
    added some simple comments to demonstrate a commit

--- TriangleAnalyzer.cs
+++ TriangleAnalyzer.cs
@@ -8,6 +8,7 @@ namespace TriangleAnalyzer
 {
     public class TriangleAnalyzer
     {
+        //returns true if the sides evaluate to a valid triangle
         public bool IsTriangle(int a, int b, int c){
             if ((a + b) > c && (b + c) > a && (c + a) > b)
                 return true;
@@ -15,6 +16,7 @@ namespace TriangleAnalyzer
                 return false;
         }
 
+        //Returns the type of triangle
         public string ClassifyTriangle(int a, int b, int c){
             if ((a == b && b == c) && IsTriangle(a,b,c))
                 return "equilateral";
